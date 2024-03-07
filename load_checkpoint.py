e660fe36340d344b2fe4544ae34fe55fb11e919d
hsmith917 <89433847+hsmith917@users.noreply.github.com>
2024-03-07 15:06:36 -0500

    initial

---
+++ load_checkpoint.py
@@ -0,0 +1,16 @@
+import random
+from typing import Dict, List, Mapping, Tuple, Set
+
+from entity_gym.env import *
+from enn_trainer import load_checkpoint, RogueNetAgent
+
+from TensorRTS import TensorRTS
+
+if __name__ == "__main__": # This is to load a model
+    # Change the checkpoint to your own folder name.
+    agent = RogueNetAgent(checkpoint.state.agent)
+    env = TensorRTS()
+    obs = env.reset()
+    action, predicted_return = agent.act(obs)    
+    print(f"ACTION: {action}")
+    print(f"Predicted_return: {predicted_return}")
